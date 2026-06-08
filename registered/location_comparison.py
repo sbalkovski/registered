7f25795aee34b1cd55cd989eae9e6578cd9136de
ayan-mbta <ayan@mbta.com>
2026-06-08 16:20:13 -0400

    fixed lint

--- registered/location_comparison.py
+++ registered/location_comparison.py
@@ -1,73 +1,75 @@
-"""
-CLI tool to compare locations between the export and TransitMaster.
-"""
+# DEPRECATED
 
-import argparse
-from pyproj import Geod
-from registered.rating import Rating
-from registered.db import geo_node
+# """
+# CLI tool to compare locations between the export and TransitMaster.
+# """
 
-GEOD = Geod(ellps="WGS84")
+# import argparse
+# from pyproj import Geod
+# from registered.rating import Rating
+# from registered.db import geo_node
 
-
-def google_street_view_url(lat, lon):
-    """
-    Generate a Google Street View URL for a given lat/lon.
-
-    Documentation:
-    https://developers.google.com/maps/documentation/urls/get-started#street-view-action
-    """
-    return f"https://www.google.com/maps/@?api=1&map_action=pano&viewpoint={lat},{lon}"
+# GEOD = Geod(ellps="WGS84")
 
 
-def main(args):
-    """
-    Entrypoint for the CLI tool.
-    """
-    rating = Rating(args.DIR)
+# def google_street_view_url(lat, lon):
+#     """
+#     Generate a Google Street View URL for a given lat/lon.
 
-    stops = {
-        stop.stop_id: stop
-        for stop in rating["nde"]
-        if stop.easting_ft and stop.northing_ft
-    }
-    stop_ids = set(stops)
-    tm_stop_locations = {
-        stop_id: (lat, lon)
-        for (stop_id, _, lat, lon) in geo_node(stop_ids)
-        if lat and lon
-    }
-    print(
-        "stop_id,stop_name,hastus_lat,hastus_lon,tm_lat,tm_lon,distance_m,"
-        "hastus_street_view,tm_street_view"
-    )
-    for stop_id in sorted(stop_ids, key=int):
-        stop = stops[stop_id]
-        (lat, lon) = stop.latlon()
-
-        if stop_id in tm_stop_locations:
-            (tm_lat, tm_lon) = tm_stop_locations[stop_id]
-            (_, _, distance) = GEOD.inv(lon, lat, tm_lon, tm_lat)
-            distance = f"{int(distance)}"
-        else:
-            distance = ""
-        print(
-            f"{stop_id},{stop.name},"
-            f"{lat:.6f},{lon:.6f},{tm_lat:.6f},{tm_lon:.6f},{distance},"
-            f'"{google_street_view_url(lat,lon)}"',
-            f'"{google_street_view_url(tm_lat,tm_lon)}"',
-        )
+#     Documentation:
+#     https://developers.google.com/maps/documentation/urls/get-started#street-view-action
+#     """
+#     return f"https://www.google.com/maps/@?api=1&map_action=pano&viewpoint={lat},{lon}"
 
 
-parser = argparse.ArgumentParser(
-    description="Compare stops between HASTUS export and TransitMaster"
-)
-parser.add_argument(
-    "DIR", help="The Combine directory where the current rating files live"
-)
+# def main(args):
+#     """
+#     Entrypoint for the CLI tool.
+#     """
+#     rating = Rating(args.DIR)
+
+#     stops = {
+#         stop.stop_id: stop
+#         for stop in rating["nde"]
+#         if stop.easting_ft and stop.northing_ft
+#     }
+#     stop_ids = set(stops)
+#     tm_stop_locations = {
+#         stop_id: (lat, lon)
+#         for (stop_id, _, lat, lon) in geo_node(stop_ids)
+#         if lat and lon
+#     }
+#     print(
+#         "stop_id,stop_name,hastus_lat,hastus_lon,tm_lat,tm_lon,distance_m,"
+#         "hastus_street_view,tm_street_view"
+#     )
+#     for stop_id in sorted(stop_ids, key=int):
+#         stop = stops[stop_id]
+#         (lat, lon) = stop.latlon()
+
+#         if stop_id in tm_stop_locations:
+#             (tm_lat, tm_lon) = tm_stop_locations[stop_id]
+#             (_, _, distance) = GEOD.inv(lon, lat, tm_lon, tm_lat)
+#             distance = f"{int(distance)}"
+#         else:
+#             distance = ""
+#         print(
+#             f"{stop_id},{stop.name},"
+#             f"{lat:.6f},{lon:.6f},{tm_lat:.6f},{tm_lon:.6f},{distance},"
+#             f'"{google_street_view_url(lat,lon)}"',
+#             f'"{google_street_view_url(tm_lat,tm_lon)}"',
+#         )
 
 
-if __name__ == "__main__":
-    import sys
+# parser = argparse.ArgumentParser(
+#     description="Compare stops between HASTUS export and TransitMaster"
+# )
+# parser.add_argument(
+#     "DIR", help="The Combine directory where the current rating files live"
+# )
 
-    sys.exit(main(parser.parse_args()))
+
+# if __name__ == "__main__":
+#     import sys
+
+#     sys.exit(main(parser.parse_args()))

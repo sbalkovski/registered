652dff9b5cd759cf1d711d0107d697989f277aec
ayan-mbta <ayan@mbta.com>
2026-06-08 10:57:07 -0400

    commented out depracated features/tests

--- registered/intervals/calculation.py
+++ registered/intervals/calculation.py
@@ -1,95 +1,97 @@
-"""
-Calculation of a fastest/shortest path for a given Interval.
-"""
+# DEPRACATED
 
-from typing import Optional, List
-import attr
-import osmnx as ox
-from .routing import RestrictedGraph
-from .interval import Stop, Interval, IntervalType
+# """
+# Calculation of a fastest/shortest path for a given Interval.
+# """
 
-Path = List[int]
+# from typing import Optional, List
+# import attr
+# import osmnx as ox
+# from .routing import RestrictedGraph
+# from .interval import Stop, Interval, IntervalType
 
-
-@attr.define(kw_only=True)
-class IntervalCalculation:
-    """
-    One interval calculation.
-    """
-
-    interval: Interval
-    fastest_path: Optional[Path] = attr.ib(default=None)
-    shortest_path: Optional[Path] = attr.ib(default=None)
-
-    def is_located(self):
-        """
-        Return True if the given interval has locations.
-        """
-        return self.interval.is_located()
-
-    @property
-    def from_stop(self) -> Stop:
-        "Return the from_stop of the Interval."
-        return self.interval.from_stop
-
-    @property
-    def to_stop(self) -> Stop:
-        "Return the to_stop of the Interval."
-        return self.interval.to_stop
-
-    @property
-    def interval_type(self) -> str:
-        "Return the formatted type of the Interval: Revenue, Deadhead, &c."
-        return str(self.interval.type.name).title()
-
-    @property
-    def description(self) -> str:
-        "Return the description of the Interval."
-        return self.interval.description or ""
-
-    @classmethod
-    def calculate(
-        cls, interval: Interval, graph: RestrictedGraph
-    ) -> "IntervalCalculation":
-        """
-        Calculate the fastest/shortest paths, given an Interval.
-        """
-        fastest_path = shortest_path = None
-        if should_calculate(interval):
-            ox.utils.log(
-                f"calculating interval from {interval.from_stop} to {interval.to_stop}"
-            )
-            fastest_path = graph.shortest_path(
-                interval.from_stop.point, interval.to_stop.point
-            )
-            if fastest_path is not None:
-                shortest_path = graph.shortest_path(
-                    interval.from_stop.point, interval.to_stop.point, weight="length"
-                )
-
-        if fastest_path == shortest_path:
-            shortest_path = None
-
-        return cls(
-            interval=interval,
-            fastest_path=fastest_path,
-            shortest_path=shortest_path,
-        )
-
-    def paths(self) -> List[Path]:
-        """
-        Return the list of unique paths in this calculation.
-        """
-        return [
-            path for path in [self.fastest_path, self.shortest_path] if path is not None
-        ]
+# Path = List[int]
 
 
-def should_calculate(interval: Interval) -> bool:
-    """
-    Return True if we should calculate a path for the given Interval.
+# @attr.define(kw_only=True)
+# class IntervalCalculation:
+#     """
+#     One interval calculation.
+#     """
 
-    - do not calculate Revenue intervals
-    - do not calculate intervals which do not have locations
-    """
-    return interval.type != IntervalType.REVENUE and interval.is_located()
+#     interval: Interval
+#     fastest_path: Optional[Path] = attr.ib(default=None)
+#     shortest_path: Optional[Path] = attr.ib(default=None)
+
+#     def is_located(self):
+#         """
+#         Return True if the given interval has locations.
+#         """
+#         return self.interval.is_located()
+
+#     @property
+#     def from_stop(self) -> Stop:
+#         "Return the from_stop of the Interval."
+#         return self.interval.from_stop
+
+#     @property
+#     def to_stop(self) -> Stop:
+#         "Return the to_stop of the Interval."
+#         return self.interval.to_stop
+
+#     @property
+#     def interval_type(self) -> str:
+#         "Return the formatted type of the Interval: Revenue, Deadhead, &c."
+#         return str(self.interval.type.name).title()
+
+#     @property
+#     def description(self) -> str:
+#         "Return the description of the Interval."
+#         return self.interval.description or ""
+
+#     @classmethod
+#     def calculate(
+#         cls, interval: Interval, graph: RestrictedGraph
+#     ) -> "IntervalCalculation":
+#         """
+#         Calculate the fastest/shortest paths, given an Interval.
+#         """
+#         fastest_path = shortest_path = None
+#         if should_calculate(interval):
+#             ox.utils.log(
+#                 f"calculating interval from {interval.from_stop} to {interval.to_stop}"
+#             )
+#             fastest_path = graph.shortest_path(
+#                 interval.from_stop.point, interval.to_stop.point
+#             )
+#             if fastest_path is not None:
+#                 shortest_path = graph.shortest_path(
+#                     interval.from_stop.point, interval.to_stop.point, weight="length"
+#                 )
+
+#         if fastest_path == shortest_path:
+#             shortest_path = None
+
+#         return cls(
+#             interval=interval,
+#             fastest_path=fastest_path,
+#             shortest_path=shortest_path,
+#         )
+
+#     def paths(self) -> List[Path]:
+#         """
+#         Return the list of unique paths in this calculation.
+#         """
+#         return [
+#             path for path in [self.fastest_path, self.shortest_path] if path is not None
+#         ]
+
+
+# def should_calculate(interval: Interval) -> bool:
+#     """
+#     Return True if we should calculate a path for the given Interval.
+
+#     - do not calculate Revenue intervals
+#     - do not calculate intervals which do not have locations
+#     """
+#     return interval.type != IntervalType.REVENUE and interval.is_located()
--- tests/intervals/test_routing.py
+++ tests/intervals/test_routing.py
@@ -1,148 +1,150 @@
-from itertools import product
-from registered.intervals import routing
-import pytest
-from pytest import approx
-from shapely.geometry import Point
-import networkx as nx
-import osmnx as ox
+# intervals.routing DEPRACATED
+
+# from itertools import product
+# from registered.intervals import routing
+# import pytest
+# from pytest import approx
+# from shapely.geometry import Point
+# import networkx as nx
+# import osmnx as ox
 
 
-def setup_module():
-    """
-    Setup for all tests in this module.
-    """
-    routing.configure_osmnx(log_console=True)
+# def setup_module():
+#     """
+#     Setup for all tests in this module.
+#     """
+#     routing.configure_osmnx(log_console=True)
 
 
-# add the below to a test case to write a map with the given path
-# # graph.folium_map(bc_high_school, federal_st, [path]).save("map.html")
+# # add the below to a test case to write a map with the given path
+# # # graph.folium_map(bc_high_school, federal_st, [path]).save("map.html")
 
 
-def assert_has_path(origin, dest, graph=None, weight="travel_time"):
-    """
-    Create a shortest path between origin and dest, and assert that it has non-zero length.
-    """
-    if graph is None:
-        graph = routing.RestrictedGraph.from_points([origin, dest])
-    path = graph.shortest_path(origin, dest, weight=weight)
-    assert path is not None
-    assert graph.path_length(path) > 0
-    return (graph, path)
+# def assert_has_path(origin, dest, graph=None, weight="travel_time"):
+#     """
+#     Create a shortest path between origin and dest, and assert that it has non-zero length.
+#     """
+#     if graph is None:
+#         graph = routing.RestrictedGraph.from_points([origin, dest])
+#     path = graph.shortest_path(origin, dest, weight=weight)
+#     assert path is not None
+#     assert graph.path_length(path) > 0
+#     return (graph, path)
 
 
-def test_no_left_turn():
-    bc_high_school = Point(-71.04149, 42.31760)
-    federal_st = Point(-71.056411, 42.355286)
-    bad_node = 61403875  # left turn from morrisey onto day blvd
-    bad_from_edge = 541034265
-    bad_to_edge = 645350240
+# def test_no_left_turn():
+#     bc_high_school = Point(-71.04149, 42.31760)
+#     federal_st = Point(-71.056411, 42.355286)
+#     bad_node = 61403875  # left turn from morrisey onto day blvd
+#     bad_from_edge = 541034265
+#     bad_to_edge = 645350240
 
-    (graph, path) = assert_has_path(bc_high_school, federal_st)
+#     (graph, path) = assert_has_path(bc_high_school, federal_st)
 
-    assert graph.path_length(path) == approx(5732, abs=1)
-    # first and last path entries are psuedo-nodes, snapped from the given
-    # shortest-path points
-    assert path[1] in [5845347051, 7718883175]
-    assert path[-2] in [61340621, 7637468345, 1202432362]
+#     assert graph.path_length(path) == approx(5732, abs=1)
+#     # first and last path entries are psuedo-nodes, snapped from the given
+#     # shortest-path points
+#     assert path[1] in [5845347051, 7718883175]
+#     assert path[-2] in [61340621, 7637468345, 1202432362]
 
-    if bad_node in path:
-        index = path.index(bad_node)
-        previous_node = path[index - 1]
-        next_node = path[index + 1]
-        previous_edge = graph.graph.edges[previous_node, bad_node, 0]
-        next_edge = graph.graph.edges[bad_node, next_node, 0]
-        assert (
-            previous_edge["osmid"] != bad_from_edge
-            and next_edge["osmid"] != bad_to_edge
-        ), f"bad turn, coming from {previous_node} going to {next_node}"
-
-
-def test_no_storrow_drive():
-    kenmore_busway = Point(-71.09583, 42.348927)
-    mass_general = Point(-71.0692, 42.3609)
-    (graph, path) = assert_has_path(kenmore_busway, mass_general)
-    for from_node, to_node in zip(path, path[1:]):
-        edge = graph.graph.edges[from_node, to_node, 0]
-        if "name" not in edge:
-            continue
-        edge_name = edge["name"]
-        assert edge_name != "Storrow Drive", f"from: {from_node}, to: {to_node}"
+#     if bad_node in path:
+#         index = path.index(bad_node)
+#         previous_node = path[index - 1]
+#         next_node = path[index + 1]
+#         previous_edge = graph.graph.edges[previous_node, bad_node, 0]
+#         next_edge = graph.graph.edges[bad_node, next_node, 0]
+#         assert (
+#             previous_edge["osmid"] != bad_from_edge
+#             and next_edge["osmid"] != bad_to_edge
+#         ), f"bad turn, coming from {previous_node} going to {next_node}"
 
 
-@pytest.mark.parametrize(
-    "points",
-    [
-        [],
-        [Point(0, 0)],
-    ],
-)
-def test_empty_graph(points):
-    with pytest.raises(routing.EmptyGraph):
-        routing.RestrictedGraph.from_points(points)
+# def test_no_storrow_drive():
+#     kenmore_busway = Point(-71.09583, 42.348927)
+#     mass_general = Point(-71.0692, 42.3609)
+#     (graph, path) = assert_has_path(kenmore_busway, mass_general)
+#     for from_node, to_node in zip(path, path[1:]):
+#         edge = graph.graph.edges[from_node, to_node, 0]
+#         if "name" not in edge:
+#             continue
+#         edge_name = edge["name"]
+#         assert edge_name != "Storrow Drive", f"from: {from_node}, to: {to_node}"
 
 
-def test_short_path():
-    point = Point(-71.171963, 42.271777)
-    graph = routing.RestrictedGraph.from_points([point])
-    path = graph.shortest_path(Point(0, 0), Point(0, 0))
-    assert len(path) == 1
+# @pytest.mark.parametrize(
+#     "points",
+#     [
+#         [],
+#         [Point(0, 0)],
+#     ],
+# )
+# def test_empty_graph(points):
+#     with pytest.raises(routing.EmptyGraph):
+#         routing.RestrictedGraph.from_points(points)
 
 
-class TestRouting:
-    OD_PAIRS = [
-        ((-71.084983, 42.39193), (-71.078666, 42.386049)),
-        ((-70.99272, 42.418574), (-70.99205, 42.413385)),
-        ((-71.101815, 42.436141), (-71.076315, 42.384167)),
-        ((-71.1292, 42.39702), (-71.129116, 42.396704)),
-        ((-71.129116, 42.396704), (-71.1292, 42.39702)),
-        ((-70.94560, 42.46236), (-70.94726, 42.46206)),
-    ]
-
-    @pytest.mark.parametrize("pair", OD_PAIRS)
-    def test_has_path(self, pair):
-        (origin, dest) = pair
-        origin_pt = Point(origin)
-        dest_pt = Point(dest)
-        (graph, _path) = assert_has_path(origin_pt, dest_pt, weight="travel_time")
-        (_graph, path) = assert_has_path(
-            origin_pt, dest_pt, graph=graph, weight="length"
-        )
+# def test_short_path():
+#     point = Point(-71.171963, 42.271777)
+#     graph = routing.RestrictedGraph.from_points([point])
+#     path = graph.shortest_path(Point(0, 0), Point(0, 0))
+#     assert len(path) == 1
 
 
-class TestMultiplePoints:
-    ORIGINS = [(-71.03991910663855, 42.33306759993236), (-71.04149, 42.31760)]
-    DESTS = [
-        (-71.056411, 42.355286),
-        (-71.03599235273778, 42.335613467448354),
-    ]
-    OD_PAIRS = list(product(ORIGINS, DESTS))
+# class TestRouting:
+#     OD_PAIRS = [
+#         ((-71.084983, 42.39193), (-71.078666, 42.386049)),
+#         ((-70.99272, 42.418574), (-70.99205, 42.413385)),
+#         ((-71.101815, 42.436141), (-71.076315, 42.384167)),
+#         ((-71.1292, 42.39702), (-71.129116, 42.396704)),
+#         ((-71.129116, 42.396704), (-71.1292, 42.39702)),
+#         ((-70.94560, 42.46236), (-70.94726, 42.46206)),
+#     ]
 
-    @classmethod
-    def setup_class(cls):
-        points = [Point(point) for point in cls.ORIGINS + cls.DESTS]
-        cls.graph = routing.RestrictedGraph.from_points(points)
-
-    @pytest.mark.parametrize("pair", OD_PAIRS)
-    def test_multiple_points(self, pair):
-        (origin, dest) = pair
-        origin_pt = Point(origin)
-        dest_pt = Point(dest)
-        (_graph, _path) = assert_has_path(origin_pt, dest_pt, graph=self.graph)
+#     @pytest.mark.parametrize("pair", OD_PAIRS)
+#     def test_has_path(self, pair):
+#         (origin, dest) = pair
+#         origin_pt = Point(origin)
+#         dest_pt = Point(dest)
+#         (graph, _path) = assert_has_path(origin_pt, dest_pt, weight="travel_time")
+#         (_graph, path) = assert_has_path(
+#             origin_pt, dest_pt, graph=graph, weight="length"
+#         )
 
 
-class TestFoliumMap:
-    OD_PAIRS = [
-        ((-71.084983, 42.39193), (-71.078666, 42.386049)),
-        ((-70.894718, 42.254173), (-70.892174, 42.251347)),
-    ]
+# class TestMultiplePoints:
+#     ORIGINS = [(-71.03991910663855, 42.33306759993236), (-71.04149, 42.31760)]
+#     DESTS = [
+#         (-71.056411, 42.355286),
+#         (-71.03599235273778, 42.335613467448354),
+#     ]
+#     OD_PAIRS = list(product(ORIGINS, DESTS))
 
-    @pytest.mark.parametrize("pair", OD_PAIRS)
-    def test_folium_map(self, pair):
-        (origin, dest) = pair
-        origin_pt = Point(origin)
-        dest_pt = Point(dest)
-        (graph, fast_path) = assert_has_path(origin_pt, dest_pt)
-        (graph, short_path) = assert_has_path(origin_pt, dest_pt, graph=graph)
-        folium_map = graph.folium_map(origin_pt, dest_pt, [fast_path, short_path])
-        assert folium_map is not None
-        assert folium_map._repr_html_()
+#     @classmethod
+#     def setup_class(cls):
+#         points = [Point(point) for point in cls.ORIGINS + cls.DESTS]
+#         cls.graph = routing.RestrictedGraph.from_points(points)
+
+#     @pytest.mark.parametrize("pair", OD_PAIRS)
+#     def test_multiple_points(self, pair):
+#         (origin, dest) = pair
+#         origin_pt = Point(origin)
+#         dest_pt = Point(dest)
+#         (_graph, _path) = assert_has_path(origin_pt, dest_pt, graph=self.graph)
+
+
+# class TestFoliumMap:
+#     OD_PAIRS = [
+#         ((-71.084983, 42.39193), (-71.078666, 42.386049)),
+#         ((-70.894718, 42.254173), (-70.892174, 42.251347)),
+#     ]
+
+#     @pytest.mark.parametrize("pair", OD_PAIRS)
+#     def test_folium_map(self, pair):
+#         (origin, dest) = pair
+#         origin_pt = Point(origin)
+#         dest_pt = Point(dest)
+#         (graph, fast_path) = assert_has_path(origin_pt, dest_pt)
+#         (graph, short_path) = assert_has_path(origin_pt, dest_pt, graph=graph)
+#         folium_map = graph.folium_map(origin_pt, dest_pt, [fast_path, short_path])
+#         assert folium_map is not None
+#         assert folium_map._repr_html_()

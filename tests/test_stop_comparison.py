e91e89a0a1096912bdab689e959d60dd938e5aa6
ayan-mbta <ayan@mbta.com>
2026-06-09 12:26:56 -0400

    remove db access test

--- tests/test_stop_comparison.py
+++ tests/test_stop_comparison.py
@@ -2,19 +2,17 @@ import pytest
 from pathlib import Path
 from registered.stop_comparison import *
 
-NEXT_RATING_PATH = Path(__file__).parent / "support" / "rating_next" / "Combine"
-PREV_RATING_PATH = Path(__file__).parent / "support" / "rating_prev" / "Combine"
+RATING_PATH = Path(__file__).parent / "support" / "stop_comparison"
 
-def test_stop_comparison_output(capsys):
-    # Create a fake argparse Namespace
-    args = argparse.Namespace(
-        CURRENT=PREV_RATING_PATH,
-        NEXT=NEXT_RATING_PATH
-    )
+def test_by_stop():
 
-    main(args)
-
-    actual_output = capsys.readouterr().out
-    expected_output_file_path = Path(__file__).parent / "support" / "stop_comparison" / "valid_stop_comparison.txt"
-
-    assert actual_output.strip() == expected_output_file_path.read_text(encoding="utf-16").strip()
+    keys = ['64','1','2','6','110']
+    values = [{('01', 'Outbound')},
+              {('01', 'Outbound')},
+              {('01', 'Outbound')},
+              {('01', 'Outbound')},
+              {('01', 'Outbound')}]
+    expected = dict(zip(keys, values)).items()
+    rating = Rating(RATING_PATH)
+    output = route_direction_by_stops(rating).items()
+    assert output == expected

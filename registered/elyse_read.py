c62476b3cbcde35f4b3ced5d95a27ebeb25f2433
Balkovski <SBalkovski@mbta.com>
2026-08-19 10:48:57 -0400

    pylint updates

--- registered/elyse_read.py
+++ registered/elyse_read.py
@@ -1,9 +1,10 @@
-## ELYSE 2 HEADSIGN PDF -> TXT CONVERSION
+"""
+ELYSE 2 HEADSIGN PDF -> TXT CONVERSION
+"""
 
-import pdfplumber
+import os
 import pandas as pd
-import os 
-from pathlib import Path
+import pdfplumber
 
 def get_delims(text):
     """ 
@@ -11,12 +12,13 @@ def get_delims(text):
     Accounts for blank spaces in "Route" column  
     """
     text_list = text.split(' ', maxsplit = 2)
-    if not (all([x.isnumeric() for x in text_list[:2]])):
+    if not all(x.isnumeric() for x in text_list[:2]):
         text_list = [text_list[0], None,  ' '.join(text_list[1:])]
     return text_list
 
 
-def parse_elyse2(file, input_loc, output_loc, export = False, output_file_name ='elyse2_headsigns.txt'):
+def parse_elyse2(file, input_loc, output_loc,
+                 export = False, output_file_name ='elyse2_headsigns.txt'):
     # Reads in pdf
     """
         Reads in PDF, returns text as a dataframe
@@ -33,18 +35,18 @@ def parse_elyse2(file, input_loc, output_loc, export = False, output_file_name =
         # extract text from each page
         extracted = p.extract_text().split('\n')
 
-        extracted = [get_delims(x) for x in extracted           # Split text into rows from get_delims function
-                                                                # EXCLUDE
-                if x.split(' ')[0] not in ['File:', 'Led']     #   Each page has extra two lines with file info
-                and len(x.split(' '))!=1                       #   Remove page number
-                and not x.startswith('Entry Route')]     #   Remove column headers
+        # Split text into rows from get_delims function
+        extracted = [get_delims(x) for x in extracted
+                if x.split(' ')[0] not in ['File:', 'Led']
+                and len(x.split(' '))!=1
+                and not x.startswith('Entry Route')]
         to_df += extracted
 
-    # Formatting outputs 
+    # Formatting outputs
     elyse2 = pd.DataFrame(to_df)
     elyse2.columns = ['Signcode', 'Route', 'Headsign']
 
-    # Combine route and front 
+    # Combine route and front
     elyse2['Headsign'] = (elyse2['Route'] + ' ').fillna('') + elyse2['Headsign'].fillna('')
 
     elyse2 = elyse2[['Signcode', 'Headsign']]
@@ -52,7 +54,6 @@ def parse_elyse2(file, input_loc, output_loc, export = False, output_file_name =
     if export:
         output_file = os.path.join(output_loc, output_file_name)
         print(f'exporting file to {output_loc}')
-        elyse2.to_csv(output_file_name, index = False, sep = '\t')
+        elyse2.to_csv(output_file, index = False, sep = '\t')
 
     return elyse2
-
